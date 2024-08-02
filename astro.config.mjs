90d6ece2b8a7878e3436c405335ef1f0a19e38f0
Ukpa Uchechi <ukpagraceu@gmail.com>
2024-08-02 16:15:20 -0700

    added base url

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ukpagrace.github.io',
+  base: '/portfolio',
   integrations: [mdx(), sitemap(), tailwind()]
 });
