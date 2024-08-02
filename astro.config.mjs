5227dcc2273b350ad935a6c96c5230c992db1ab3
Ukpa Uchechi <ukpagraceu@gmail.com>
2024-08-02 15:30:44 -0700

    added git pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astrofy-template.netlify.app',
+  site: 'https://ukpagrace.github.io',
+  base: '/portfolio',
   integrations: [mdx(), sitemap(), tailwind()]
 });
