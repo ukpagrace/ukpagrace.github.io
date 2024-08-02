ba5c09e8362f26a40b21fc668ccf6d1d3b15e9a4
Ukpa Uchechi <ukpagraceu@gmail.com>
2024-08-02 15:14:07 -0700

    modified UI

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astrofy | Personal Portfolio Website Template';
-export const SITE_DESCRIPTION = 'Astrofy is a free and open-source template for your Personal Portfolio Website built with Astro and TailwindCSS. Create in minutes a website with Blog, CV, Project Section, Store and RSS Feed.';
+export const SITE_TITLE = 'Portfolio | Personal Portfolio Website';
+export const SITE_DESCRIPTION = 'Welcome to my personal portfolio website, showcasing my projects, skills, and professional journey. Explore my work, discover my expertise, and learn more about my passion for computers. Connect with me and see how I can bring value to your next project.';
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true
